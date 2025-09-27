90ff64b28d57430e4d206985933448f7974277a2
David Taylor <jdavidtaylor1@outlook.com>
2025-09-27 14:21:02 -0700

    Add dir-locals and ACL2 customization to atomic stobjs

---
+++ books/projects/atomic-stobjs/.dir-locals.el
@@ -0,0 +1,73 @@
+((lisp-mode . (
+               ;; Do not insert tabs when indenting.
+               (indent-tabs-mode . nil)
+               ;; Auto-wrap lines longer than 80 characters.
+               (fill-column . 80)
+               ;; Enforce ACL2 single-semicolon block comment convention.
+               (comment-column . 0)
+               (comment-fill-column . 80)
+               ;; Ensure `BEGINNING-OF-DEFUN' (called in `ACL2-BEGINNING-OF-DEF' in
+               ;; emacs-acl2.el) finds the top-level of a form rather than the most
+               ;; recent line-initiating open-parenthesis.
+               (open-paren-in-column-0-is-defun-start . nil)
+               (defun-prompt-regexp . nil)
+               ;; Maintain Emacs defaults.
+               (lisp-indent-offset . nil)
+               (comment-padding . " ")
+
+;;; Common Lisp
+               ;; Use Common Lisp instead of Emacs Lisp indentation for `IF'.
+               (eval . (put 'if 'common-lisp-indent-function 3))
+               ;; NOTE: `COMMON-LISP-INDENT-FUNCTION' has hard-coded indentation
+               ;; for any form beginning with "loop", and the match is
+               ;; case-insensitive.  This causes Emacs to misindent the body of
+               ;; the `LOOP$' macro.  Moreover, this bug cannot be resolved by
+               ;; setting the `COMMON-LISP-INDENT-FUNCTION' property of `LOOP$'.
+
+;;; ACL2 built-ins
+               (eval . (put 'defabsstobj 'common-lisp-indent-function 1))
+               (eval . (put 'deflabel 'common-lisp-indent-function 0))
+               (eval . (put 'defpkg 'common-lisp-indent-function 1))
+               (eval . (put 'defstobj 'common-lisp-indent-function 1))
+               (eval . (put 'deftheory 'common-lisp-indent-function 1))
+               (eval . (put 'deftheory-static 'common-lisp-indent-function 1))
+               (eval . (put 'defthm 'common-lisp-indent-function 1))
+               (eval . (put 'defthmd 'common-lisp-indent-function 1))
+               (eval . (put 'defxdoc 'common-lisp-indent-function 1))
+               (eval . (put 'encapsulate 'common-lisp-indent-function 1))
+               (eval . (put 'er-progn 'common-lisp-indent-function 0))
+               (eval . (put 'exists 'common-lisp-indent-function 1))
+               (eval . (put 'forall 'common-lisp-indent-function 1))
+               (eval . (put 'in-theory 'common-lisp-indent-function 0))
+               (eval . (put 'include-book 'common-lisp-indent-function 0))
+               (eval . (put 'local 'common-lisp-indent-function 0))
+               (eval . (put 'make-event 'common-lisp-indent-function 0))
+               (eval . (put 'mv-let 'common-lisp-indent-function '(8 8 &body)))
+               (eval . (put 'stobj-let 'common-lisp-indent-function '(11 11 11 &body)))
+               (eval . (put 'thm 'common-lisp-indent-function 0))
+               (eval . (put 'verify-guards 'common-lisp-indent-function 1))
+               (eval . (put 'with-output 'common-lisp-indent-function 0))
+
+;;; Community Books
+               (eval . (put 'b* 'common-lisp-indent-function 1))
+               (eval . (put 'def-b*-binder 'common-lisp-indent-function 1))
+               (eval . (put 'definition-free-theory 'common-lisp-indent-function '(1)))
+               (eval . (put 'definition-theory 'common-lisp-indent-function '(1)))
+               (eval . (put 'defsection 'common-lisp-indent-function 1))
+               (eval . (put 'defun-theory 'common-lisp-indent-function '(1)))
+
+;;; Atomic-Stobjs
+               (eval . (put 'with-books 'common-lisp-indent-function 1))
+
+               ))
+ (shell-mode . (
+                ;; Fix `BEGINNING-OF-DEFUN' (see above comment).
+                (open-paren-in-column-0-is-defun-start . nil)
+                (defun-prompt-regexp . nil)
+                ))
+ ;; Ensure Emacs opens files in the appropriate major mode.
+ (auto-mode-alist . (
+                     ("\\.acl2\\'" . lisp-mode)
+                     ("\\.lisp\\'" . lisp-mode)
+                     ("\\.lsp\\'" . lisp-mode)
+                     )))
