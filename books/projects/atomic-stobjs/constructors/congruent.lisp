1c27f95f321b80d00fae6a56815b7aeb7611fd86
David Taylor <jdavidtaylor1@outlook.com>
2025-11-07 18:52:47 -0700

    Define congruent stobj constructor

---
+++ books/projects/atomic-stobjs/constructors/congruent.lisp
@@ -0,0 +1,76 @@
+;;; Copyright 2025 J. David Taylor
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;;
+;;; 1. Redistributions of source code must retain the above copyright notice,
+;;;    this list of conditions and the following disclaimer.
+;;;
+;;; 2. Redistributions in binary form must reproduce the above copyright notice,
+;;;    this list of conditions and the following disclaimer in the documentation
+;;;    and/or other materials provided with the distribution.
+;;;
+;;; 3. Neither the name of the copyright holder nor the names of its
+;;;    contributors may be used to endorse or promote products derived from this
+;;;    software without specific prior written permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+
+
+;;;; Prologue
+(in-package "ATOMIC-STOBJS")
+(set-verify-guards-eagerness 2)
+
+(include-book "../utilities/symbolicate")
+
+
+;;;; `DEFINE-CONGRUENT'
+(defmacro define-congruent (stobj &key
+                                    (executable 'nil)
+                                    (debug 'nil))
+  (declare (xargs :guard (and (symbolp stobj)
+                              stobj
+                              (booleanp executable)
+                              (booleanp debug))))
+
+  `(with-output
+     ,@(and (not debug)
+            *constructor-output*)
+
+     (make-event
+       (let* ((stobj ',stobj)
+              (executable ',executable)
+              (%stobj (symbolicate stobj "%" stobj))
+              (stobj-property (getpropc stobj 'acl2::stobj))
+              (absstobj-info (getpropc stobj 'acl2::absstobj-info))
+              (recognizer-export (assoc (caadr stobj-property) (cdr absstobj-info)))
+              (recognizer-export (list (symbolicate stobj "%" (first recognizer-export))
+                                       :logic (second recognizer-export)
+                                       :exec (third recognizer-export)))
+              (creator-export (assoc (cdadr stobj-property) (cdr absstobj-info)))
+              (creator-export (list (symbolicate stobj "%" (first creator-export))
+                                    :logic (second creator-export)
+                                    :exec (third creator-export)))
+              (foundation (car absstobj-info))
+              (exports (loop$ :for entry :in (cdddr absstobj-info)
+                             :collect (list (symbolicate stobj "%" (first entry))
+                                            :logic (second entry)
+                                            :exec (third entry)))))
+
+         `(defabsstobj ,%stobj
+            :foundation ,foundation
+            :congruent-to ,stobj
+            :recognizer ,recognizer-export
+            :creator ,creator-export
+            :non-executable ,(not executable)
+            :exports ,exports)))))
